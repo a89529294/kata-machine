eee154cfe3f8fb19d60344a7c6c7ec289310d59a
Albert Chang <a89529294@hotmail.com>
2022-09-30 19:51:21 +0800

    feat: bs search done

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,11 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let l = 0;
+    let h = haystack.length;
+    do {
+        const m = Math.floor(l + (h - l) / 2);
+        if (haystack[m] === needle) return true;
+        else if (haystack[m] < needle) l = m + 1;
+        else h = m;
+    } while (l < h);
+    return false;
 }
