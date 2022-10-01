4806c02083d5758ae4212a3c44d584b5b02f4bbd
Albert Chang <a89529294@hotmail.com>
2022-10-01 11:01:17 +0800

    feat: finish TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,27 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const l = breaks.length;
+    const d = Math.floor(Math.sqrt(l));
+    for (let i = d - 1; i < l; i += d) {
+        if (breaks[i]) {
+            for (let j = i - d + 1; j < i + 1; j++) {
+                if (breaks[j]) return j;
+            }
+        }
+    }
+    return -1;
 }
+
+// export default function two_crystal_balls(breaks: boolean[]): number {
+//     const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+//     let i = jmpAmount;
+//     for (; i < breaks.length; i += jmpAmount) {
+//         if (breaks[i]) break;
+//     }
+
+//     i -= jmpAmount;
+//     for (let j = 0; j < jmpAmount + 1 && i < breaks.length; i++, j++) {
+//         if (breaks[i]) return i;
+//     }
+//     return -1;
+// }
